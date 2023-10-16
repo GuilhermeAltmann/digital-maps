e38ad07d09234a35602bdbbfbd74ecbf55dbc926
Guilherme Altmann <altmannguilherme@gmail.com>
2023-10-16 19:49:30 -0300

    adicionando migrate para criação da tabela pinterest

--- database/migrations/2023_10_16_000001_create_pinterest.php
+++ database/migrations/2023_10_16_000001_create_pinterest.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pinterest', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('x');
+            $table->integer('y');
+            $table->integer('opened_hours');
+            $table->integer('opened_minutes');
+            $table->integer('closed_hours');
+            $table->integer('closed_minutes');
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pinterest');
     }
 };
