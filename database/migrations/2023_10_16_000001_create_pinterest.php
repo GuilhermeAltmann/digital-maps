2aa50ccd16bbd13bd11be6521c1dcc53fb1ddfd5
Guilherme Altmann <altmannguilherme@gmail.com>
2023-10-17 20:23:03 -0300

    alterando nome da tabela

--- database/migrations/2023_10_16_000001_create_pinterest.php
+++ database/migrations/2023_10_16_000001_create_pinterest.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pinterest', function (Blueprint $table) {
+        Schema::create('pinterests', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('x');
