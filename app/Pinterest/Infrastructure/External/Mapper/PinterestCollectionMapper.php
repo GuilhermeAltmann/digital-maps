2876b1a60ca94d1367cd09e41ab46f93b2082ef0
Guilherme Altmann <altmannguilherme@gmail.com>
2023-10-19 17:12:17 -0300

    removendo nullable do pinterest collection mapper

--- app/Pinterest/Infrastructure/External/Mapper/PinterestCollectionMapper.php
+++ app/Pinterest/Infrastructure/External/Mapper/PinterestCollectionMapper.php
@@ -8,21 +8,19 @@ use App\Pinterest\Domain\ValueObject\OpeningHours;
 
 abstract class PinterestCollectionMapper
 {
-    public static function map(?array $array): PinterestCollection
+    public static function map(array $array): PinterestCollection
     {
         $collection = [];
-        if ($array) {
-            foreach ($array as $data) {
-                $collection[] = Pinterest::create(
-                    $data['name'],
-                    $data['x'],
-                    $data['y'],
-                    $data['opened'],
-                    $data['closed'],
-                    OpeningHours::DEFAULT_FORMAT_MYSQL,
-                    OpeningHours::DEFAULT_FORMAT_MYSQL
-                );
-            }
+        foreach ($array as $data) {
+            $collection[] = Pinterest::create(
+                $data['name'],
+                $data['x'],
+                $data['y'],
+                $data['opened'],
+                $data['closed'],
+                OpeningHours::DEFAULT_FORMAT_MYSQL,
+                OpeningHours::DEFAULT_FORMAT_MYSQL
+            );
         }
 
         return PinterestCollection::create($collection);
