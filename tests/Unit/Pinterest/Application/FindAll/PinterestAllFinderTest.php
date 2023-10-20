47d4d87face778cc81d8d82d3ecc4ab8d558a5cc
Guilherme Altmann <altmannguilherme@gmail.com>
2023-10-19 21:57:49 -0300

    adicionando teste

--- tests/Unit/Pinterest/Application/FindAll/PinterestAllFinderTest.php
+++ tests/Unit/Pinterest/Application/FindAll/PinterestAllFinderTest.php
@@ -6,7 +6,6 @@ use App\Pinterest\Application\FindAll\PinterestAllFinder;
 use App\Pinterest\Domain\Pinterest;
 use App\Pinterest\Domain\PinterestCollection;
 use App\Pinterest\Domain\Repository\FindAllPinterestRepository;
-use App\Pinterest\Infrastructure\External\Mapper\PinterestCollectionMapper;
 use Mockery;
 use Tests\TestCase;
 
@@ -14,14 +13,6 @@ class PinterestAllFinderTest extends TestCase
 {
     public function test_should_return_pinterest_collection(): void
     {
-        $pinterestCollection = PinterestCollectionMapper::map([[
-            'name' => 'teste',
-            'x' => 12,
-            'y' => 10,
-            'opened' => '12:00',
-            'closed' => '15:00',
-        ]]);
-
         $findAllPinterestRepository = Mockery::mock(FindAllPinterestRepository::class);
         $findAllPinterestRepository->shouldReceive('findAllPinterest')
             ->andReturn(PinterestCollection::create([
